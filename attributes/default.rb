9d6e70c8635a74265f8ab5cd7a32c3aee28fe8ba
Pavel Suchman <pavel.suchman@gmail.com>
2014-04-28 15:43:17 +0300

    Updated paths

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 default['copy-cookbooks']['cache_dir'] = "/var/cache/chef/cache/cookbooks/"
 default['copy-cookbooks']['staging_dir'] = "/tmp/packer-chef-solo"
+default['copy-cookbooks']['cookbooks_dir'] = node['copy-cookbooks']['staging_dir'] + "/cookbooks-0"
 
