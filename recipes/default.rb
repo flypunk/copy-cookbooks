c2c82e56f483394242d7e108298aa2911f7c2df1
Pavel Suchman <pavel.suchman@gmail.com>
2014-04-28 14:38:37 +0300

    Copy the CONTENTS of staging dir

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,6 +10,6 @@ bash "Copy cookbooks from packer chef solo staging directory" do
 
     code <<-EOH
     mkdir -p #{node['copy-cookbooks']['cache_dir']}
-    cp -a #{node['copy-cookbooks']['staging_dir']} #{node['copy-cookbooks']['cache_dir']}
+    cp -a #{node['copy-cookbooks']['staging_dir']}/* #{node['copy-cookbooks']['cache_dir']}
     EOH
 end
